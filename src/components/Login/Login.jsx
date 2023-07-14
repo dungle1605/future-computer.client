15303971201f5e2a071439ba69d2a64bc33a66d5
Le Tien Dung <tien.dung.le@dksh.com>
2023-07-14 13:03:59 +0700

    Add code for Login and Register module

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 import { Link } from "react-router-dom";
 import "./Login.css";
-import PropTypes from "prop-types";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
-Login.propTypes = {};
+function Login() {
+  const navigate = useNavigate();
+  const { REACT_APP_API_ENDPOINT } = process.env;
+  const displayTime = 3000;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-function Login(props) {
+  const command = { Username: username, password: password };
+
+  const handleUserLogin = (e) => {
+    e.preventDefault();
+
+    return axios
+      .post(`${REACT_APP_API_ENDPOINT}/api/user/login`, command)
+      .then((res) => {});
+  };
+
   return (
     <div className="container">
-      <form action="#">
+      <ToastContainer
+        position="top-right"
+        autoClose={displayTime}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover={false}
+        theme="light"
+      ></ToastContainer>
+      <form onSubmit={handleUserLogin}>
         <div className="title">Login</div>
         <div className="input-box underline">
-          <input type="text" placeholder="Enter Your Email" required />
+          <input
+            type="text"
+            placeholder="Enter Your Username"
+            required
+            onChange={(e) => setUsername(e)}
+            value={username}
+          />
           <div className="underline"></div>
         </div>
         <div className="input-box">
-          <input type="password" placeholder="Enter Your Password" required />
+          <input
+            type="password"
+            placeholder="Enter Your Password"
+            required
+            onChange={(e) => setPassword(e)}
+            value={password}
+          />
           <div className="underline"></div>
         </div>
         <div className="input-box button">
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,18 +1,69 @@
 import React, { useState } from "react";
 import "./Register.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 function Register() {
+  const displayTime = 3000;
+  const navigate = useNavigate();
+  const { REACT_APP_API_ENDPOINT } = process.env;
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [retypePass, setRetypePass] = useState("");
+
+  const handleRegisterAccount = (e) => {
+    e.preventDefault();
+
+    if (password != retypePass) {
+      toast.error("Password is missmatch");
+      return;
+    }
+
+    const command = { UserName: username, Email: email, Password: password };
+
+    return axios
+      .post(`${REACT_APP_API_ENDPOINT}/api/user/register`, command)
+      .then((res) => {
+        toast.success(res.data);
+        setTimeout(() => {
+          navigate("/login");
+        }, displayTime);
+      })
+      .catch((err) => toast.error(err.message));
+  };
 
   return (
     <div className="container">
-      <form action="#">
-        <div className="title">Login</div>
+      <ToastContainer
+        position="top-right"
+        autoClose={displayTime}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable={false}
+        pauseOnHover={false}
+        theme="light"
+      ></ToastContainer>
+      <form onSubmit={handleRegisterAccount}>
+        <div className="title">Register</div>
         <div className="input-box underline">
           <input
             type="text"
+            placeholder="Enter Your Username"
+            required
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
+          <div className="underline"></div>
+        </div>
+        <div className="input-box underline">
+          <input
+            type="email"
             placeholder="Enter Your Email"
             required
             onChange={(e) => setEmail(e.target.value)}
@@ -31,7 +82,13 @@ function Register() {
           <div className="underline"></div>
         </div>
         <div className="input-box">
-          <input type="password" placeholder="Retype Your Password" required />
+          <input
+            type="password"
+            placeholder="Retype Your Password"
+            required
+            onChange={(e) => setRetypePass(e.target.value)}
+            value={retypePass}
+          />
           <div className="underline"></div>
         </div>
         <div className="input-box button">
